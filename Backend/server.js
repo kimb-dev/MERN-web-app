c2f26614b9a6e9484929974f971581098c0150c3
Kim Berhanu <141489735+kberhanu@users.noreply.github.com>
2025-01-04 22:38:42 +1100

    get all, update & delete product api calls added

--- Backend/server.js
+++ Backend/server.js
@@ -2,12 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json()); //allows json data in req.body to be accepted
+
+app.get("/api/products", async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({success: true, data: products});
+    } catch (error) {
+        console.log("Error in fetching products")
+        res.status(500).json({success: false, message: "Server Error"});
+    }
+})
 
 app.post("/api/products", async (req, res) => {
     const product = req.body; //user sends data
@@ -26,6 +37,36 @@ app.post("/api/products", async (req, res) => {
         res.status(500).json({success:false, message: "Server Error"});
     }
 });
+
+app.delete("/api/products/:id", async(req, res) => {
+    const {id} = req.params
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success: true, message: "Product deleted"});
+    } catch (error) {
+        console.log("Error deleting product:", error.message);
+        res.status(404).json({success: false, message: "Product not found"});
+        
+    }
+});
+
+app.put("/api/products/:id", async (req, res) => {
+    const {id} = req.params;
+    const product = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({success: false, message: "Product not found"});
+    }
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new:true});
+        res.status(200).json({success: true, data: updatedProduct});
+    } catch (error) {
+        console.log("Error updating product", error);
+        res.status(500).json({success: false, message: "Server Error"});
+        
+    }
+})
 
 app.listen(5000, () => {
     connectDB();
