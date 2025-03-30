e5781e6ca0bdc8a9e029f818d8a08c3927697baf
Kim Berhanu <141489735+kberhanu@users.noreply.github.com>
2025-03-30 21:40:13 +1100

    Navbar, Add Product icon, Home and Create pages added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,20 @@
-import { Button, Box } from "@chakra-ui/react";
+import { Box } from '@chakra-ui/react';
+import { Route, Routes } from 'react-router-dom';
+import CreatePage from "./pages/CreatePage";
+import HomePage from "./pages/HomePage";
+import Navbar from './components/navbar';
 
 function App() {
   console.log("✅ App component rendered");
   return (
-    <Box p={6} bg="gray.100" minH="100vh">
-      <Button colorScheme="teal" size="lg">
-        Hello from Chakra ✨
-      </Button>
+    <Box minH={"100vh"}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
     </Box>
+
   );
 }
 
